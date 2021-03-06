549f5773c8b1c6fdd293a809aa69e44e5f593ac5
Taylor Arnold <taylorj.arnold2013@hotmail.com>
2021-03-05 20:50:39 -0800

    update to code

--- Flask/app.py
+++ Flask/app.py
@@ -19,11 +19,33 @@ def home():
         age = float(request.args.get("age")) or 0
         work_type = request.args.get("work_type")
         smoking_status = request.args.get("smoking_status")
-        avg_glucose_level = float(request.args.get("agl")) or 0
+        avg_glucose_level = float(request.args.get("avg_glucose_level")) or 0
+        hypertension = request.args.get("hypertension")
         if int(age) > 60: 
             output += 0.25
         if smoking_status == "smokes":
+            output += 0.75
+        elif smoking_status == "formerly_smoked":
             output += 0.50
+        elif smoking_status == "never smoked":
+            output += 0.0
+        elif smoking_status == "Unknown":
+            output += 0.25
+        if work_type == "Self_employed":
+            output += 0.75
+        elif work_type == "Private":
+            output += 0.50
+        elif work_type == "Govt_job":
+            output += 0.25
+        elif work_type == "Never_worked":
+            output += 0.0
+        if int(avg_glucose_level) > 100:
+            output += 0.50
+        print(hypertension)
+        if hypertension == "on":
+            output += 0.75
+        else: 
+            output += 0.0
 
     if output < 0.50:
         return render_template('index.html', prediction_text='The patient is NOT likely to have a Stroke')
